ebfc9828efbe3b4e7923c380721818f6c0ee0cee
Sachin Ranchod <sachin.ranchod@gmail.com>
2013-05-12 20:18:08 +0200

    add sendgrid config

--- app.rb
+++ app.rb
@@ -129,7 +129,17 @@ class EventNotifier
       Pony.mail :to => 'sachin@ranchod.co.za',
         :from => 'sachin@ranchod.co.za',
         :subject => 'UFC event reminder',
-        :body => erb(:'emails/notification')
+        :body => erb(:'emails/notification'),
+        :via => :smtp,
+        :via_options => {
+          :address => 'smtp.sendgrid.net',
+          :port => '587',
+          :domain => 'heroku.com',
+          :user_name => ENV['SENDGRID_USERNAME'],
+          :password => ENV['SENDGRID_PASSWORD'],
+          :authentication => :plain,
+          :enable_starttls_auto => true
+        }
     end
   end
 end
